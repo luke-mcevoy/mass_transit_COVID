a08100d1b8587673a5eb542c3965e0c8dab68500
Luke McEvoy <43505529+luke-mcevoy@users.noreply.github.com>
2020-05-24 20:25:07 -0400

    Update algorithms.py

--- mass_transit_COVID/algorithms.py
+++ mass_transit_COVID/algorithms.py
@@ -5,19 +5,24 @@
 
 import math
 
+# Checks if all the passengers have been seated w/ bool return type
 def everyone_seated(seats, seated, passengers):
 	if seated == passengers:
 		return True
 	return False
 
+# First pattern design.
 def A_dynamic(rows, cols, passengers, seats):
 	extra_rows = (rows - passengers)
 	seated = 0
+	# tmp represents the amount to incrementation due to extra rows
 	tmp = 0
 	for curr_row in range(0, rows):
 		curr_col = (seated % 2) * (cols - 1)
+		# If seat is open, seat next passenger in queue here
 		if seats[curr_row + tmp][curr_col] == 0:
 			seats[curr_row + tmp][curr_col] = 1
+			# Increment seated variable
 			seated += 1
 			if extra_rows > 0:
 				tmp += (extra_rows // passengers) + 1
@@ -25,14 +30,18 @@ def A_dynamic(rows, cols, passengers, seats):
 			if (everyone_seated(seats, seated, passengers)):
 				break
 
+# Second pattern design.
 def B_dynamic(rows, cols, passengers, seats):
 	extra_rows = (rows - passengers)
 	seated = 0
+	# tmp represents the amount to incrementation due to extra rows
 	tmp = 0
 	for curr_row in range(0, rows):
 		curr_col = (seated % 2) * (cols - 1)
+		# If seat is open, seat next passenger in queue here
 		if seats[curr_row + tmp][curr_col] == 0:
 			seats[curr_row + tmp][curr_col] = 1
+			# Increment seated variable
 			seated += 1
 			if extra_rows > 0:
 				tmp += 1
@@ -44,6 +53,7 @@ def C_dynamic(rows, cols, passengers, seats):
 	extra_rows = rows - (passengers * (2/3)) // 1
 	seated = 0
 	tmp = 0
+	# tmp represents the amount to incrementation due to extra rows
 	for curr_row in range(0, rows):
 		# current row is even
 		if (curr_row % 2) == 0:
@@ -71,6 +81,7 @@ def C_dynamic(rows, cols, passengers, seats):
 def D_dynamic(rows, cols, passengers, seats):
 	extra_rows = rows - math.ceil(passengers//2)
 	seated = 0
+	# tmp represents the amount to incrementation due to extra rows
 	tmp = 0
 	for curr_row in range(0, rows):
 		if seats[curr_row + tmp][0] == 0:
