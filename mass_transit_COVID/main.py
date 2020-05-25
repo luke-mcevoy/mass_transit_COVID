d19422aa36834eaa4537cdcef8d103cf406f243f
Luke McEvoy <43505529+luke-mcevoy@users.noreply.github.com>
2020-05-24 20:38:20 -0400

    Update main.py

--- mass_transit_COVID/main.py
+++ mass_transit_COVID/main.py
@@ -1,16 +1,23 @@
+# Title: 	main.py
+# Author: 	Luke McEvoy
+# Created: 	May 18, 2020
+# Summary:	Optimized space between passengers on mass transit
+
 from gateway import *
 from algorithms import *
 
+# User inputs
 rows = int(input("Rows: "))
 cols = int(input("Columns: "))
 passengers = int(input("Passengers: "))
+
+# Declaration of global 2D array seats. Used as seating chart.
 seats = [[0 for i in range(cols)] for j in range(rows)]
 
 capacity_percentage = round(passengers / (rows * cols), 2)
 print("Capacity percentage: " + str(capacity_percentage))
 
-seats = [[0 for i in range(cols)] for j in range(rows)]
-
+# If statements to appropriate function in regards to column sizes.
 if (cols == 1):
 	single(rows, cols, passengers, seats, capacity_percentage)
 elif (cols == 2):
@@ -22,6 +29,7 @@ elif (rows < passengers):
 else:
 	triple(rows, cols, passengers, seats, capacity_percentage)
 
+# Output final seating chart
 for x in seats:
 	print(x)
 
